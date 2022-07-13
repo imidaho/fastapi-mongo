52edba70eaa3ee9f1d486580590e2de18420effe
imidaho <jamesmcochran@gmail.com>
2022-07-13 10:08:59 -0400

    My FastApi and MongoDB Application

---
+++ app/server/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from app.server.routes.student import router as StudentRouter
+
+app = FastAPI()
+
+app.include_router(StudentRouter, tags=["Student"], prefix="/student")
+
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Hello World...And Cat!"}
+
+
